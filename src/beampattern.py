3a8a428f0ab68c850be7ba4fac89eb5a46c6fe38
Fabio Daniel <68433060+masterAristocats@users.noreply.github.com>
2026-06-29 01:45:06 -0300

    Update beampattern.py

--- src/beampattern.py
+++ src/beampattern.py
@@ -21,8 +21,8 @@ def beampattern(
     azimuth : float ou array_like
         Azimute(s) em radianos.
 
-    elevation : float
-        Elevação em radianos.
+    elevation : float ou array_like
+        Elevação(ões) em radianos.
 
     wavelength : float
         Comprimento de onda.
@@ -51,29 +51,59 @@ def beampattern(
             )
 
     azimuth = np.atleast_1d(azimuth)
+    elevation = np.atleast_1d(elevation)
 
-    array_factor = np.zeros(azimuth.size, dtype=complex)
-
-    for i, phi in enumerate(azimuth):
-
-        a = steering_vector(
-            positions,
-            phi,
-            elevation,
-            wavelength
+    # Apenas um dos ângulos pode ser um vetor nesta versão
+    if azimuth.size > 1 and elevation.size > 1:
+        raise ValueError(
+            "Nesta versão, apenas azimuth OU elevation pode ser um vetor."
         )
 
-        array_factor[i] = np.conjugate(weights) @ a
+    # ===============================
+    # Varredura em azimute (UCA)
+    # ===============================
+    if azimuth.size > 1:
+
+        array_factor = np.zeros(azimuth.size, dtype=complex)
+
+        for i, phi in enumerate(azimuth):
+
+            a = steering_vector(
+                positions,
+                phi,
+                elevation.item(),
+                wavelength
+            )
+
+            array_factor[i] = np.conjugate(weights) @ a
+
+    # ===============================
+    # Varredura em elevação (ULA)
+    # ===============================
+    else:
+
+        array_factor = np.zeros(elevation.size, dtype=complex)
+
+        for i, theta in enumerate(elevation):
+
+            a = steering_vector(
+                positions,
+                azimuth.item(),
+                theta,
+                wavelength
+            )
+
+            array_factor[i] = np.conjugate(weights) @ a
 
     # Magnitude do Array Factor
     gain = np.abs(array_factor)
 
+    # Normalização
     max_gain = np.max(gain)
 
     if max_gain == 0:
         raise ValueError("O ganho máximo é igual a zero.")
 
-    # Normalização
     gain /= max_gain
 
     # Conversão para dB
