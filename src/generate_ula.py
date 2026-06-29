36afe525aa7616c56bf336d05ebe6b9ad51918b9
Fabio Daniel <68433060+masterAristocats@users.noreply.github.com>
2026-06-29 01:08:02 -0300

    Update generate_ula.py

--- src/generate_ula.py
+++ src/generate_ula.py
@@ -1,9 +1,9 @@
 import numpy as np
 
+
 def generate_ula(M, d):
     """
-    Gera as coordenadas tridimensionais de um
-    Uniform Linear Array (ULA).
+    Gera as coordenadas tridimensionais de um Uniform Linear Array (ULA).
 
     Parâmetros
     ----------
@@ -14,8 +14,9 @@ def generate_ula(M, d):
 
     Retorna
     -------
-    positions : ndarray (M, 3)
-        Coordenadas (x, y, z) dos sensores.
+    numpy.ndarray
+        Matriz de dimensão (M, 3) contendo as coordenadas (x, y, z)
+        dos sensores.
     """
 
     if M <= 0:
@@ -30,3 +31,13 @@ def generate_ula(M, d):
     positions[:, 0] = np.arange(M) * d
 
     return positions
+
+
+if __name__ == "__main__":
+    M = 8
+    d = 0.5
+
+    positions = generate_ula(M, d)
+
+    print("Posições dos sensores:")
+    print(positions)
